1d7c110e75a1e429993cdfe2439992b2f21821a2
kostas <kopapaioan@gmail.com>
2021-02-12 15:56:55 +0200

    Added Todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,74 @@
 <script>
-	export let name;
+	import { slide } from "svelte/transition";
+	import { elasticInOut } from "svelte/easing";
+
+	let todos = [];
+	let input = "";
+
+	function addTodo() {
+		if (input) {
+			todos = [
+				...todos,
+				{
+					text: input,
+					id: Math.random()
+						.toString(36)
+						.substr(2, 9)
+				}
+			];
+		}
+		input = "";
+	}
+
+	function removeTodo(id) {
+		const index = todos.findIndex(todo => todo.id === id);
+		todos.splice(index, 1);
+		todos = todos;
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+	<script src="https://kit.fontawesome.com/49105198bc.js" crossorigin="anonymous"></script>
+</svelte:head>
+
+<main class="container is-fluid">
+  <div class="columns is-centered is-vcentered is-mobile">
+    <div class="column is-narrow" style="width: 70%">
+      <h1 class="has-text-centered title">Tasks</h1>
+      <form class="field has-addons" style="justify-content: center" on:submit|preventDefault={addTodo}>
+        <div class="control">
+          <input bind:value={input} class="input" type="text" placeholder="TODO">
+        </div>
+        <div class="control">
+          <button class="button is-primary">
+            <span class="icon is-small">
+              <i class="fas fa-plus"></i>
+            </span>
+          </button>
+        </div>
+      </form>
+      <ul class:list={todos.length > 0}>
+        {#each todos as todo (todo.id)}
+          <li class="list-item" transition:slide="{{duration: 300, easing: elasticInOut}}">
+            <div class="is-flex" style="align-items: center">
+              <span class="is-pulled-left">{todo.text}</span>
+              <div style="flex: 1"></div>
+              <button class="button is-text is-pulled-right is-small" on:click={()=> removeTodo(todo.id)}>
+                <span class="icon">
+                  <i class="fas fa-check"></i>
+                </span>
+              </button>
+            </div>
+          </li>
+        {:else}
+          <li class="has-text-centered" transition:slide="{{delay: 600, duration: 300, easing: elasticInOut}}">Nothing here!</li>
+        {/each}
+      </ul>
+    </div>
+  </div>
 </main>
+
 
 <style>
 	main {
